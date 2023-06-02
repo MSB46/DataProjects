2982005062814ec622adb01c6336580f27318038
msb46 <michaelsaulonb@gmail.com>
2023-06-02 15:00:04 -0400

    Modified approach to data processing and more

--- Animation_Fascination/FlaskAPI/app.py
+++ Animation_Fascination/FlaskAPI/app.py
@@ -1,13 +1,14 @@
 import flask
 from flask import Flask, jsonify, request
 import json
-from app_input import data_in
+from app_input_votescore import data_in
+from app_input_gross import data_in
 import numpy as np
 import pickle
 
 def load_models():
-    file_name = "models/model_file.p"
-    with open(file_name, 'rb') as pickled:
+    file_name_g = "models/model_file_gross.p"
+    with open(file_name_g, 'rb') as pickled:
         data = pickle.load(pickled)
         model = data['model']
     return model
